15d8e6bd02b7e4957f1c812025e859ccc0213855
ZABBIX <zabbix@ztrunk.space>
2024-03-23 17:15:53 +0100

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,16 +6,9 @@ from pypulse.Template import Template
 # Specifying application Route
 Aplication.Vars.APLICATION_PATH = os.path.dirname(os.path.abspath(__file__))
 
-# Defining the locations for templates and static files.
-Template.TEMPLATE_PATH = os.path.join(
-    Aplication.Vars.APLICATION_PATH, 'templates')
-Template.STATIC_PATH = os.path.join(
-    Aplication.Vars.APLICATION_PATH, 'static')
-
-
 # Configuring applications
 # If you create a new application, make sure to include it here.
-Aplication.SetAplication('baseapp')
+APPLICATIONS = ["baseapp"]
 
 # Application window settings
 APP_SETTINGS = {
@@ -27,6 +20,16 @@ APP_SETTINGS = {
     'icon_path': os.path.join(
         Aplication.Vars.APLICATION_PATH, 'window_logo.ico')
 }
+
+# Defining the locations for templates and static files.
+Template.TEMPLATE_PATH = os.path.join(
+    Aplication.Vars.APLICATION_PATH, 'templates')
+Template.STATIC_PATH = os.path.join(
+    Aplication.Vars.APLICATION_PATH, 'static')
+
+# setting apps
+for x in APPLICATIONS:
+    Aplication.SetAplication(x)
 
 # Initializing window
 browser = Window.LoadBrowser(**APP_SETTINGS)
